e5a53434838b63838f6bba9c77871de70b285d06
Kim woo jong <skekdnwhd@gmail.com>
2024-09-10 00:21:28 +0900

    tailwind 적용, title favicon 변경

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="w-full max-w-sm bg-white p-6 rounded-lg shadow-lg">
+      <HelloWorld msg="Wedding Invitation" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
