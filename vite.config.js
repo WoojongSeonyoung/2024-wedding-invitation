3dd5cd4f65dec26547b945807640ae99abdb86d0
kimwoojong <skekdnwhd@gmail.com>
2024-09-10 16:51:31 +0900

    Chore : full url setting

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    'process.env': {}
-  },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
