1e58a7bc39b80881ab6e3b45246a84350128b220
Kim woo jong <skekdnwhd@gmail.com>
2024-09-12 00:47:36 +0900

    글자체 수정

--- src/components/QuoteCard.vue
+++ src/components/QuoteCard.vue
@@ -12,10 +12,10 @@ const formatContent = (text) => {
 
 <template>
   <div class="bg-gray-50 p-5 rounded-lg shadow-lg max-w-md mx-auto mt-4">
-    <h2 class="noto-sans-kr-regular text-lg text-gray-950 font-bold mb-2 overflow-hidden text-ellipsis whitespace-nowrap">
+    <h2 class="text-lg text-gray-950 font-bold mb-2 overflow-hidden text-ellipsis whitespace-nowrap">
       {{ title }}
     </h2>
-    <p class="noto-sans-kr-regular text-sm text-gray-800" v-html="formatContent(content)">
+    <p class="text-sm text-gray-800" v-html="formatContent(content)">
     </p>
   </div>
 </template>
