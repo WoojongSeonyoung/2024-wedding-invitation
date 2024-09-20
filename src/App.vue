dc92e3b54066d99b7442fcaa25209585d72eba4c
Kim woo jong <skekdnwhd@gmail.com>
2024-09-21 00:41:28 +0900

    remove Noise animation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="outer">
     <main class="app relative">
-<!--      <CinemaNoise />-->
       <SnowFlakes icon="favorite" />
       <router-view></router-view>
       <AudioButton />
@@ -12,7 +11,6 @@
 <script setup>
 import AudioButton from '@/components/AudioButton.vue';
 import SnowFlakes from '@/components/SnowFlakes.vue';
-import CinemaNoise from '@/components/CinemaNoise.vue';
 
 </script>
 
