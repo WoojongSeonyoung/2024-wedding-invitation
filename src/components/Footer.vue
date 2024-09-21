b959cf875c9850a07089996e1bdd3bf2eb629991
Kim woo jong <skekdnwhd@gmail.com>
2024-09-22 01:39:35 +0900

    뮤직 변경에 따른 제작자 삭제

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,17 +8,5 @@
         <img src="@/assets/icon/github-mark.svg" alt="icon" class="inline h-4 w-4">
       </a>
     </p>
-    <p class="text-sm text-gray-400 popins-regular">
-      Music by
-      <a
-          class="popins-regular"
-          href="https://pixabay.com/ko/users/paulyudin-27739282/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=182505">Pavel
-        Bekirov</a> from
-      <a
-          class="popins-regular"
-          href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=182505">
-        Pixabay
-      </a>
-    </p>
   </footer>
 </template>
